88c6db2ff67d787418d96886a7db2c59afbf683e
Jonathan Weekes <jweekes13@live.com>
2024-07-30 21:11:55 -0400

    Added another python file to repository called decorators

---
+++ decorators.py
@@ -0,0 +1,12 @@
+'''
+A decorator is a function that recieves a function as input and returns a modified version of that 
+function or its output as a result.
+'''
+
+# Explicit Example of a decorator or wrapper function
+def announce(f):
+    def wrapper():
+        print("About to run the function....")
+        # Execute the function
+        f()
+        print("Done with the function")
