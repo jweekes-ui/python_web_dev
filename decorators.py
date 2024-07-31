e8c632482576322b1b8d128b7bc5aa6c3d32d4d8
Jonathan Weekes <jweekes13@live.com>
2024-07-30 22:02:27 -0400

    Updated decorators.py - Implemented decorator in example

--- decorators.py
+++ decorators.py
@@ -5,9 +5,18 @@ function or its output as a result.
 
 # Explicit Example of a decorator or wrapper function
 def announce(f):
-    def wrapper():
+    def wrapper(name):
         print("About to run the function....")
         # Execute the function
-        f()
+        f(name)
         print("Done with the function")
-        return wrapper
+    return wrapper
+
+# Implementing the decorator
+
+@announce
+def hello(name):
+    print(f"Hello {name}!")
+
+# Calling the f() function hello()
+hello("John")
